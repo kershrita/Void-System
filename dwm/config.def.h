ad72a4dabffcb19cadfff6e67e5ab5921148b109
Ashraf Abdulkhaliq <ashrafabdulkhaliq80@gmail.com>
2023-10-09 04:36:05 +0300

    change dwm font to FontAwosem

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,8 +7,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int splitstatus        = 0;        /* 1 for split status items */
 static const char *splitdelim        = ";";       /* Character used for separating status */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "FontAwesome:size=10" };
+static const char dmenufont[]       = "FontAwesome:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -713,8 +713,6 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, wdelta, tw = 0;
-	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
