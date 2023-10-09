793a998c5a40cd245205cb8ca96abdfa2fa9efe2
Ashraf Abdulkhaliq <ashrafabdulkhaliq80@gmail.com>
2023-10-09 04:34:57 +0300

    add bettary capacity to status bar

--- scripts/statusbar.sh
+++ scripts/statusbar.sh
@@ -89,6 +89,41 @@ volume_alsa() {
 	fi
 }
 
+bettary_capacity() {
+	case $BLOCK_BUTTON in
+		3) notify-send "🔋 Battery module" "🔋: discharging
+	🛑: not charging
+	♻: stagnant charge
+	🔌: charging
+	⚡: charged
+	❗: battery very low!
+	- Scroll to change adjust xbacklight." ;;
+		4) xbacklight -inc 10 ;;
+		5) xbacklight -dec 10 ;;
+		6) "$TERMINAL" -e "$EDITOR" "$0" ;;
+	esac
+
+	# Loop through all attached batteries and format the info
+	for battery in /sys/class/power_supply/BAT?*; do
+		# If non-first battery, print a space separator.
+		[ -n "${capacity+x}" ] && printf " "
+		# Sets up the status and capacity
+		case "$(cat "$battery/status" 2>&1)" in
+			"Full") status="⚡" ;;
+			"Discharging") status="🔋" ;;
+			"Charging") status="🔌" ;;
+			"Not charging") status="🛑" ;;
+			"Unknown") status="♻️" ;;
+			*) exit 1 ;;
+		esac
+		capacity="$(cat "$battery/capacity" 2>&1)"
+		# Will make a warn variable if discharging and low
+		[ "$status" = "🔋" ] && [ "$capacity" -le 25 ] && warn="❗"
+		# Prints the info
+		printf "%s%s%d%%" "$status" "$warn" "$capacity"; unset warn
+	done && printf "\\n"
+
+}
 clock() {
 	dte=$(date +"%D")
 	time=$(date +"%H:%M")
@@ -98,7 +133,7 @@ clock() {
 
 main() {
 	while true; do
-		xsetroot -name " $(usbmon) $(ram) | $(cpu) | $(network) | $(volume_alsa) | $(clock)"
+		xsetroot -name " $(usbmon) $(ram) | $(cpu) | $(bettary_capacity) | $(network) | $(volume_alsa) | $(clock)"
 		sleep 1
 	done
 }
