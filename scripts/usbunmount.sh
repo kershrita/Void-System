705a025a2d5871057fc254bedc7461f80cf3cd93
Ashraf Abdulkhaliq <ashrafabdulkhaliq80@gmail.com>
2023-10-11 03:04:12 +0000

    add script for usb unmounting

---
+++ scripts/usbunmount.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Function to list available drives
+
+list_drives() {
+	lsblk -lp | awk '{print $1, "(" $4 ")"}' | grep sdb
+}
+
+# Display a list of drives using dmenu
+selected_drive=$(list_drives | dmenu -p "Select a drive:" | awk '{print $1}')
+
+sudo umount  /mnt/usb
+notify-send "Unmounted $selected_drive"
